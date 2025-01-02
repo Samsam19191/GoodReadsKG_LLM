ada8fae684aa7109e35529782105e4799dda0be9
Samsam19191 <taidersami1@gmail.com>
2025-01-02 14:44:53 +0300

    Refactor data processing and knowledge graph enrichment in main.py

--- main.py
+++ main.py
@@ -17,37 +17,37 @@ def main():
     
     # PROCESS DATA
     
-    # book_processor = DataProcessor(fileoutput="cleaned_books-small")
-    # book_processor.reset_data()
-    # book_processor.process_books(filename="book-small")
-    # book_processor.process_books(filename="book700k-800k")
-    # book_processor.process_books(filename="book1-100k")
-    # for i in range(1, 20):
-    #     book_processor.process_books(filename=f"book{i}00k-{i+1}00k")
+    book_processor = DataProcessor(fileoutput="cleaned_books-small")
+    book_processor.reset_data()
+    book_processor.process_books(filename="book-small")
+    book_processor.process_books(filename="book700k-800k")
+    book_processor.process_books(filename="book1-100k")
+    for i in range(1, 20):
+        book_processor.process_books(filename=f"book{i}00k-{i+1}00k")
 
-    # rating_processor = DataProcessor(fileoutput="cleaned_ratings")
-    # rating_processor.reset_data()
-    # for i in range(0, 6000, 1000):
-    #     rating_processor.process_ratings(filename=f"user_rating_{i}_to_{i+1000}")
-    # rating_processor.process_ratings(filename="user_rating_6000_to_11000")
+    rating_processor = DataProcessor(fileoutput="cleaned_ratings")
+    rating_processor.reset_data()
+    for i in range(0, 6000, 1000):
+        rating_processor.process_ratings(filename=f"user_rating_{i}_to_{i+1000}")
+    rating_processor.process_ratings(filename="user_rating_6000_to_11000")
 
     # DEFINE KNOWLEDGE GRAPH
     
-    # graph_creator = GraphCreator()
-    # graph_creator.connect_to_neo4j(neo4j_uri, neo4j_username, neo4j_password)
-    # graph_creator.generate_book_graph("cleaned_books-small")
-    # graph_creator.add_ratings_to_graph("cleaned_ratings")
-    # graph_creator.disconnect_from_neo4j()
+    graph_creator = GraphCreator()
+    graph_creator.connect_to_neo4j(neo4j_uri, neo4j_username, neo4j_password)
+    graph_creator.generate_book_graph("cleaned_books-small")
+    graph_creator.add_ratings_to_graph("cleaned_ratings")
+    graph_creator.disconnect_from_neo4j()
 
     # ENRICH KNOWLEDGE GRAPH WITH LLM
     
-    # LLM_graph_enrichment = LLMGraphEnrichment(
-    #     neo4j_uri, neo4j_username, neo4j_password, openai_api_key
-    # )
-    # try:
-    #     LLM_graph_enrichment.enrich_with_LLM()
-    # finally:
-    #     LLM_graph_enrichment.close()
+    LLM_graph_enrichment = LLMGraphEnrichment(
+        neo4j_uri, neo4j_username, neo4j_password, openai_api_key
+    )
+    try:
+        LLM_graph_enrichment.enrich_with_LLM()
+    finally:
+        LLM_graph_enrichment.close()
 
     # ENRICH KNOWLEDGE GRAPH WITH DBPEDIA
     
